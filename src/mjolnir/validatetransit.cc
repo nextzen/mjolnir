26b7a4fb88f2e52024079b3c5229534a3d1f34cd
gknisely <greg@mapzen.com>
2017-01-26 14:34:29 -0500

    added parsing functions.

--- src/mjolnir/validatetransit.cc
+++ src/mjolnir/validatetransit.cc
@@ -7,7 +7,9 @@
 #include <thread>
 #include <queue>
 
+#include <boost/algorithm/string/regex.hpp>
 #include <boost/filesystem/operations.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include <valhalla/midgard/logging.h>
 #include <valhalla/midgard/sequence.h>
@@ -168,6 +170,126 @@ void validate(const boost::property_tree::ptree& pt, std::mutex& lock,
 namespace valhalla {
 namespace mjolnir {
 
+std::vector<OneStopTest> ParseTestFile(const std::string& filename) {
+  typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+  boost::char_separator<char> sep{","};
+  std::vector<OneStopTest> onestoptests;
+  std::string default_date_time = baldr::DateTime::get_testing_date_time();
+
+  // Open file
+  std::string line;
+  std::ifstream file(filename);
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      tokenizer tok{line, sep};
+      uint32_t field_num = 0;
+      OneStopTest onestoptest{};
+      for (const auto &t : tok) {
+        switch (field_num) {
+          case 0:
+            onestoptest.origin = remove_parens(t);
+            break;
+          case 1:
+            onestoptest.destination = remove_parens(t);
+            break;
+          case 2:
+            onestoptest.route_id = remove_parens(t);
+            break;
+          case 3:
+            onestoptest.date_time = remove_parens(t);
+            break;
+        }
+        field_num++;
+      }
+      if (onestoptest.date_time.empty())
+        onestoptest.date_time = default_date_time;
+
+      onestoptests.emplace_back(std::move(onestoptest));
+    }
+    file.close();
+  } else {
+    std::cout << "One stop test file: " << filename << " not found" << std::endl;
+  }
+
+  return onestoptests;
+}
+
+/*
+void ParseLogFile(const std::string& filename) {
+  typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+  boost::char_separator<char> sep{" "};
+
+  std::string type, origin, dest, tranisit_route;
+  bool route=false;
+  origin = "";
+  dest = "";
+  tranisit_route = "";
+  int num = 0;
+  // Open file
+  std::string line;
+  std::ifstream file(filename);
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      tokenizer tok{line, sep};
+      uint32_t field_num = 0;
+      OneStopTest onestoptest{};
+      for (const auto &t : tok) {
+        switch (field_num) {
+          case 2:
+            type = remove_parens(t);
+            break;
+          case 3:
+            if (type == "[ANALYTICS]") {
+              auto o_s = remove_parens(t);
+              std::string transit, value;
+              std::size_t found = o_s.find_last_of("::");
+              if (found != std::string::npos) {
+                transit = o_s.substr(0,found-1);
+                value = o_s.substr(found+1);
+              }
+
+              if (transit == "transit_route_stopid") {
+                if (route) {
+                  std::cout << origin << "," << dest << "," << tranisit_route << std::endl;
+                  origin = "";
+                  dest = "";
+                  tranisit_route = "";
+                }
+                tranisit_route = value;
+                route = true;
+              } else if (transit == "transit_stopid") {
+                if (route && origin.empty())
+                  origin = value;
+                else dest = value;
+              } else if (transit == "trip_length") {
+                //std::cout << ++num << std::endl;
+              }
+            } else if (type == "[INFO]"){
+              origin = "";
+              dest = "";
+              tranisit_route = "";
+              type = "";
+              route = false;
+            } else if (route) {
+              std::cout << origin << "," << dest << "," << tranisit_route << std::endl;
+              origin = "";
+              dest = "";
+              tranisit_route = "";
+              type = "";
+              route = false;
+            }
+            break;
+        }
+        field_num++;
+      }
+    }
+    file.close();
+  } else {
+    std::cout << "One stop test file: " << filename << " not found" << std::endl;
+  }
+
+}*/
+
 // Enhance the local level of the graph
 void ValidateTransit::Validate(const boost::property_tree::ptree& pt,
                                const std::unordered_set<GraphId>& all_tiles,
@@ -178,6 +300,121 @@ void ValidateTransit::Validate(const boost::property_tree::ptree& pt,
   LOG_INFO("Validating transit network.");
 
   auto t1 = std::chrono::high_resolution_clock::now();
+  if (!all_tiles.size()) {
+    LOG_INFO("No transit tiles found. Transit will not be validated.");
+    return;
+  }
+
+  if (!onestoptests.size()) {
+    LOG_INFO("No transit tests found. Transit will not be validated.");
+    return;
+  }
+
+  // A place to hold worker threads and their results
+  std::vector<std::shared_ptr<std::thread> > threads(thread_count);
+
+  // An atomic object we can use to do the synchronization
+  std::mutex lock;
+
+  // A place to hold the results of those threads (exceptions, stats)
+  std::list<std::promise<validate_stats> > results;
+
+  // Start the threads, divvy up the work
+  LOG_INFO("Validating " + std::to_string(all_tiles.size()) + " transit tiles...");
+  size_t floor = all_tiles.size() / threads.size();
+  size_t at_ceiling = all_tiles.size() - (threads.size() * floor);
+  std::unordered_set<GraphId>::const_iterator tile_start, tile_end = all_tiles.begin();
+
+  // Atomically pass around stats info
+  for (size_t i = 0; i < threads.size(); ++i) {
+    // Figure out how many this thread will work on (either ceiling or floor)
+    size_t tile_count = (i < at_ceiling ? floor + 1 : floor);
+    // Where the range begins
+    tile_start = tile_end;
+    // Where the range ends
+    std::advance(tile_end, tile_count);
+    // Make the thread
+    results.emplace_back();
+    threads[i].reset(
+        new std::thread(validate, std::cref(pt.get_child("mjolnir")),
+                        std::ref(lock), tile_start, tile_end,
+                        std::cref(onestoptests), std::ref(results.back())));
+  }
+
+  // Wait for them to finish up their work
+  for (auto& thread : threads) {
+    thread->join();
+  }
+
+
+  // Check all of the outcomes, to see about maximum density (km/km2)
+  validate_stats stats{};
+  uint32_t failure_count = 0;
+  for (auto& result : results) {
+    // If something bad went down this will rethrow it
+    try {
+      auto thread_stats = result.get_future().get();
+      stats(thread_stats);
+      failure_count += stats.failure_count;
+    }
+    catch(std::exception& e) {
+      //TODO: throw further up the chain?
+    }
+  }
+
+  if (failure_count)
+    LOG_ERROR("There were " + std::to_string(failure_count) + " failures!");
+
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+  uint32_t secs = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+  LOG_INFO("Finished validating transit network - took " + std::to_string(secs) + " secs");
+
+}
+
+// Enhance the local level of the graph
+void ValidateTransit::Validate(const boost::property_tree::ptree& pt,
+                               const std::vector<OneStopTest>& onestoptests) {
+
+  unsigned int thread_count = std::max(static_cast<unsigned int>(1), std::thread::hardware_concurrency());
+
+  LOG_INFO("Validating transit network.");
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  std::unordered_set<GraphId> all_tiles;
+
+  // Bail if nothing
+  auto hierarchy_properties = pt.get_child("mjolnir");
+  auto transit_dir = hierarchy_properties.get_optional<std::string>("transit_dir");
+  if(!transit_dir || !boost::filesystem::exists(*transit_dir) || !boost::filesystem::is_directory(*transit_dir)) {
+    LOG_INFO("Transit directory not found. Transit will not be added.");
+    return;
+  }
+  // Also bail if nothing inside
+  transit_dir->push_back('/');
+  GraphReader reader(hierarchy_properties);
+  const auto& hierarchy = reader.GetTileHierarchy();
+  auto local_level = hierarchy.levels().rbegin()->first;
+  if(boost::filesystem::is_directory(*transit_dir + std::to_string(local_level + 1) + "/")) {
+    boost::filesystem::recursive_directory_iterator transit_file_itr(*transit_dir + std::to_string(local_level +1 ) + "/"), end_file_itr;
+    for(; transit_file_itr != end_file_itr; ++transit_file_itr) {
+      if(boost::filesystem::is_regular(transit_file_itr->path()) && transit_file_itr->path().extension() == ".gph") {
+        auto graph_id = GraphTile::GetTileId(transit_file_itr->path().string());
+        auto local_graph_id = graph_id;
+        local_graph_id.fields.level -= 1;
+        if(GraphReader::DoesTileExist(hierarchy_properties, local_graph_id)) {
+          const GraphTile* tile = reader.GetGraphTile(local_graph_id);
+          all_tiles.emplace(local_graph_id);
+          const std::string destination_path = pt.get<std::string>("mjolnir.tile_dir") + '/' + GraphTile::FileSuffix(graph_id, hierarchy);
+          boost::filesystem::path filename = destination_path;
+          // Make sure the directory exists on the system and copy to the tile_dir
+          if (!boost::filesystem::exists(filename.parent_path()))
+            boost::filesystem::create_directories(filename.parent_path());
+          boost::filesystem::copy_file(transit_file_itr->path(),destination_path,boost::filesystem::copy_option::overwrite_if_exists);
+        }
+      }
+    }
+  }
   if (!all_tiles.size()) {
     LOG_INFO("No transit tiles found. Transit will not be validated.");
     return;
